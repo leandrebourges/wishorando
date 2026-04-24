3f67b992cab0dfa47f78c262e12ee9893ca3783b
maxencels576 <maxencels576@gmail.com>
2026-04-24 11:13:13 +0200

    Modif

---
+++ page_recherche.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html> 
+
+    <head>
+        <title> Recherche de randonnées </title>
+        <meta charset = "UTF-8">
+        <link rel = "stylesheet" href = "page_recherche.css" />
+    </head> 
+
+    <body>
+
+        <header>
+            <h1> Wishorando </h1>
+        </header>
+
+        <main>
+        </main>
+
+    </body>
+
+
+</html>
