4b913a737c23714b47aabb2bfcdfb5c09f1329d6
maxencels576 <maxencels576@gmail.com>
2026-05-06 09:23:29 +0200

    Ouais

--- sortie.sql
+++ sortie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 06 mai 2026 à 08:51
+-- Généré le : mer. 06 mai 2026 à 09:21
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -45,10 +45,10 @@ CREATE TABLE `sortie` (
 --
 
 INSERT INTO `sortie` (`id`, `nom`, `depart_longitude`, `depart_latitude`, `description`, `parcours`, `distance`, `denivele`, `difficulte`, `chien_autorise`) VALUES
-(25, '10.24 km 236 m Arbusigny, La Grange, Le Biollay, Chez Boget, Penavex, Arbusigny', 6.21871, 46.0914, 'Haute Savoie, commune d\'Arbusigny 10.24 km 236 m…', '10-24-km-236-m-arbusigny-la-grange-le-biollay-chez', 10.24, 320, 'Facile', 1),
-(26, '11.25 km 403 m Cusy, Marsinge, Esery, Moussy , Vuret , Arculinge, Cusy', 6.24861, 46.134, 'Haute Savoie commune de Réignier-Esery 11.25 km 403 m…', '11-25-km-403-m-cusy-marsinge-esery-moussy-vuret-ar', 11.25, 690, 'Moyen', 1),
-(27, 'Entre rives droite et gauche du Tech', 2.6347, 42.4576, 'Au départ d\'Arles sur Tech, le circuit grimpe…', '66-entre-rives-droite-et-gauche-du-tech-en-vallesp', 66, 2000, 'Difficile', 0),
-(28, 'ABC - Sortie Locale 30/01/2011 - 40 km', 3.22028, 50.1752, 'ABC - Sortie Locale 30/01/2011 - 40 km', 'abc-sortie-locale-30-01-2011-40-km.gpx', 40, 430, 'Moyen', 0);
+(1, '10.24 km 236 m Arbusigny, La Grange, Le Biollay, Chez Boget, Penavex, Arbusigny', 6.21871, 46.0914, 'Haute Savoie, commune d\'Arbusigny 10.24 km 236 m…', '10-24-km-236-m-arbusigny-la-grange-le-biollay-chez', 10.24, 320, 'Facile', 1),
+(2, '11.25 km 403 m Cusy, Marsinge, Esery, Moussy , Vuret , Arculinge, Cusy', 6.24861, 46.134, 'Haute Savoie commune de Réignier-Esery 11.25 km 403 m…', '11-25-km-403-m-cusy-marsinge-esery-moussy-vuret-ar', 11.25, 690, 'Moyen', 1),
+(3, 'Entre rives droite et gauche du Tech', 2.6347, 42.4576, 'Au départ d\'Arles sur Tech, le circuit grimpe…', '66-entre-rives-droite-et-gauche-du-tech-en-vallesp', 66, 2000, 'Difficile', 0),
+(4, 'ABC - Sortie Locale 30/01/2011 - 40 km', 3.22028, 50.1752, 'ABC - Sortie Locale 30/01/2011 - 40 km', 'abc-sortie-locale-30-01-2011-40-km.gpx', 40, 430, 'Moyen', 0);
 
 --
 -- Index pour les tables déchargées
@@ -68,7 +68,7 @@ ALTER TABLE `sortie`
 -- AUTO_INCREMENT pour la table `sortie`
 --
 ALTER TABLE `sortie`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
