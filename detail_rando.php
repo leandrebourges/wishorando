69d1709c163ffedd8ebe20901de806c02cbe83f0
maxencels576 <maxencels576@gmail.com>
2026-05-06 10:54:48 +0200

    Lien cliquable de page_recherche à detail_rando

--- detail_rando.php
+++ detail_rando.php
@@ -2,6 +2,9 @@
 
 <?php
     include_once 'connexion_bdd.php';
+    $id = $_GET['id'];
+    $sql_rando = "SELECT * FROM sortie WHERE id = $id";
+    $result_rando = $conn->query($sql_rando);
 ?>
 
 <html>
@@ -9,7 +12,7 @@
     <head>
         <title> Page Randonnée </title>
         <meta charset = "UTF-8">
-        <link rel = "stylesheet" href = "detail_rando.css" />
+        <link rel = "stylesheet" href = "detail_rando.css"/>
     </head> 
 
     <body>
@@ -20,7 +23,19 @@
 
         <main>
 
-            
+            <div class = "box1">
+                <h2>Affichage de la randonnée sélectionnée</h2>
+                <div class = "sortie">
+                    <?php
+                        $rando = $result_rando->fetch_assoc();
+                        echo "<div class = 'rando'>Nom : {$rando['nom']}<br>
+                                        Distance : {$rando['distance']}<br>
+                                      </div>";
+                    ?>
+
+                </div>
+            </div>
+
 
         </main>
         
--- page_recherche.php
+++ page_recherche.php
@@ -2,6 +2,8 @@
 
 <?php
     include_once 'connexion_bdd.php';
+    $sql_rando = "SELECT id, nom, distance FROM sortie";
+    $result_rando = $conn->query($sql_rando);
 ?>
 
 <html> 
@@ -10,7 +12,11 @@
         <title> Recherche de randonnées </title>
         <meta charset = "UTF-8">
         <link rel = "stylesheet" href = "page_recherche.css" />
+		<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+		<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
     </head> 
+
 
     <body>
 
@@ -28,7 +34,7 @@
                             while ($rando = $result_rando->fetch_assoc()){
                                 echo "<div class = 'rando'>Nom : {$rando['nom']}<br>
                                         Distance : {$rando['distance']}<br>
-                                        <a class = 'detail' href = 'detail.php?id={$rando['id']}'>Plus de détails</a>
+                                        <a class = 'detail' href = 'detail_rando.php?id={$rando['id']}'>Plus de détails</a>
                                       </div>";
                             }
                         ?>
@@ -39,17 +45,49 @@
                 <div class = "affichage">
                     <h2>Carte : </h2>
                     <div class = "carte">
-                        <img class = "img" src = "images.jpg"/>
+                        <div id = "map" style = "width: 100%; height: 100%;"></div>
                     </div>
                 </div> 
             </div>
             
+
 
         </main>
 
         <footer>
             <?php include_once 'footer.php';?>
         </footer>
+
+
+        <!-- NE PAS TOUCHER !! Affichage de la carte ! -->
+        <script>
+
+            const map = L.map('map').setView([45.905, 6.13], 12);
+
+            const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            var marker = L.marker([45.905, 6.13]).addTo(map);
+
+            var popup = L.popup()
+            .setLatLng([45.915, 6.13])
+            .setContent("I am a standalone popup.")
+            .openOn(map);
+
+            var popup = L.popup();
+
+            function onMapClick(e) {
+                popup
+                    .setLatLng(e.latlng)
+                    .setContent("You clicked the map at " + e.latlng.toString())
+                    .openOn(map);
+            }
+
+            map.on('click', onMapClick);
+
+        </script>
 
     </body>
 
