f3187dc7f60aecb49a11ac11a6963d713e0f13b7
maxencels576 <maxencels576@gmail.com>
2026-05-06 09:38:44 +0200

    Avancement detail_rando

---
+++ detail_rando.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<?php
+    include_once 'connexion_bdd.php';
+?>
+
+<html>
+
+    <head>
+        <title> Page Randonnée </title>
+        <meta charset = "UTF-8">
+        <link rel = "stylesheet" href = "detail_rando.css" />
+    </head> 
+
+    <body>
+
+        <header>
+            <?php include_once 'barre_recherche.php';?>
+        </header>
+
+        <main>
+
+            
+
+        </main>
+        
+        
+    </body>
+</html>
